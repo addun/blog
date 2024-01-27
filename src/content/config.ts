6bfce5575cae13cff51eea72f3e7c708f98da196
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-01-27 18:41:10 +0100

    Optimize images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,25 @@ import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    id: z.string().length(6),
-    oldId: z.number(),
-    title: z.string(),
-    description: z.string(),
-    tags: z.array(reference("tags")),
-    thumbnail: z.string(),
-    repository: z.string().nullable().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.string().length(6),
+      oldId: z.number(),
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(reference("tags")),
+      thumbnail: image(),
+      repository: z.string().nullable().optional(),
+    }),
 });
 
 const tagCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      thumbnail: image(),
+    }),
 });
 
 export const collections = {
