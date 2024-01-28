6e1bfa6f2692bd2c4d0ca5e03c92ee7d86a200e3
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-01-28 21:37:33 +0100

    Add next articles

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -16,21 +16,21 @@ export function Demo({ height, repository, branch = "master" }: Props) {
         sandbox="allow-scripts allow-same-origin allow-modals allow-forms"
         height={height}
         loading="lazy"
-        className="w-full min-h-[100px] border-none"
+        className="w-full min-h-[100px] border border-blue-100 dark:border-sky-950"
       ></iframe>
-      <ul className="text-sm flex justify-end list-none p-1 my-0 bg-blue-100 rounded-b-sm">
+      <ul className="text-sm flex justify-end list-none p-1 my-0 bg-blue-100 dark:bg-sky-950 rounded-b-sm">
         <li>
           <a href={github} target="_blank" className="text-xs">
             GitHub
           </a>
         </li>
-        <li className="w-px bg-blue-700 ml-1 mr-1"></li>
+        <li className="px-2">&#x2022;</li>
         <li>
           <a href={demo} target="_blank" className="text-xs">
             New window
           </a>
         </li>
-        <li className="w-px bg-blue-700 ml-1 mr-1"></li>
+        <li className="px-2">&#x2022;</li>
         <li>
           <a href={stackBlitz} target="_blank" className="text-xs">
             StackBlitz
