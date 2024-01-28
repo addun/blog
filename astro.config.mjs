540d9eb6c6d2fb39037354f4a0622a713e928386
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-01-28 22:30:04 +0100

    Add OG

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
+import metaTags from "astro-meta-tags";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react(), sitemap()],
+  integrations: [mdx(), tailwind(), react(), sitemap(), metaTags()],
   output: "server",
   site: "https://codegen.studio",
   adapter: netlify(),
