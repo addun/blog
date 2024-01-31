022ba7eacccc8807ecb13c2674a79ef8ad1e3539
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-01-31 23:29:16 +0100

    Fix tag wrap

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function TagList({ tags }: Props) {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-wrap gap-x-4">
       {tags.map((tag) => (
         <PostTag tag={tag} key={tag} />
       ))}
