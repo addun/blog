91d0e311bd5ad7c9cdeba2121e1e75ca4a172113
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-02-05 22:35:12 +0100

    Added / at the end of URL

--- src/utils/blog-id.ts
+++ src/utils/blog-id.ts
@@ -1,5 +1,5 @@
 import type { CollectionEntry } from "astro:content";
 
 export function getBlogPostPath(entry: CollectionEntry<"blog">): string {
-  return `/blog/${entry.data.id}/${entry.slug}`;
+  return `/blog/${entry.data.id}/${entry.slug}/`;
 }
