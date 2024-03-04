26dcd54a1aec6d8e889c261978a5b689f3988211
Adrian Bury <adrian.jakub.bury@gmail.com>
2024-03-04 23:07:48 +0100

    Add provideClientHydration()

--- src/content/blog/interceptor-pattern/example.component.ts
+++ src/content/blog/interceptor-pattern/example.component.ts
@@ -1,7 +1,11 @@
 import { Component } from "@angular/core";
+import { provideClientHydration } from "@angular/platform-browser";
 
 @Component({
   standalone: true,
   template: `<h1>Test</h1>`,
 })
-export class ExampleComponent {}
+export class ExampleComponent {
+  static clientProviders = [];
+  static renderProviders = [provideClientHydration()];
+}
