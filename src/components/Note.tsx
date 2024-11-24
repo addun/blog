9ca35b8672f6230600b46c709f5b35a55696de07
addun <adrian.jakub.bury@gmail.com>
2024-11-24 17:02:45 +0100

    Edit interceptor-pattern article

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -10,17 +10,19 @@ interface Props {
   as?: React.ElementType;
 }
 
+const noteVariants = {
+  CRITICAL: "border-l-red-300 dark:border-l-red-900 bg-red-100 dark:bg-red-950",
+  HELPFUL: "border-l-sky-300 dark:border-l-sky-900 bg-sky-100 dark:bg-sky-950",
+  IMPORTANT: "border-l-yellow-300 dark:border-l-yellow-900 bg-yellow-100 dark:bg-yellow-950",
+};
+
 export function Note({ children, type, className, as: Component = "div" }: Props) {
   validator.parse(type);
 
-  const extra = {
-    CRITICAL: "border-l-red-300 dark:border-l-red-900 bg-red-100 dark:bg-red-950",
-    HELPFUL: "border-l-sky-300 dark:border-l-sky-900 bg-sky-100 dark:bg-sky-950",
-    IMPORTANT: "border-l-yellow-300 dark:border-l-yellow-900 bg-yellow-100 dark:bg-yellow-950",
-  }[type];
+  const variant = noteVariants[type];
 
   return (
-    <Component className={`px-4 py-1 my-10 rounded border-solid  border-l-8 ${extra} ${className}`}>
+    <Component className={`px-4 py-1 my-10 rounded border-solid  border-l-8 ${variant} ${className}`}>
       {children}
     </Component>
   );
